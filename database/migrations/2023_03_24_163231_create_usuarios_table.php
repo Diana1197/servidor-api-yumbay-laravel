f9356228ab77873265db176a3c65fabfadf331f5
registro <dianayumbay@gmail.com>
2023-04-10 23:22:00 -0500

    feat: change

--- database/migrations/2023_03_24_163231_create_usuarios_table.php
+++ database/migrations/2023_03_24_163231_create_usuarios_table.php
@@ -50,15 +50,15 @@ return new class extends Migration
         // Datos del administrador
 
         $data = [
-            'cedula' => '0250186665',
-            'nombres' => 'Dorian Josue',
-            'apellidos' => 'Armijos Gadvay',
-            'direccion' => 'San Miguel',
-            'celular' => '0989960587',
-            'email' => 'dorian@admin.es',
+            'cedula' => '0250194412',
+            'nombres' => 'Diana Mercedes',
+            'apellidos' => 'Yumbay Remache',
+            'direccion' => 'Santa fe',
+            'celular' => '0981044784',
+            'email' => 'diana@admin.es',
             'titulo' => 'Ing. Software',
             'horario' => 'Lunes, Martes',
-            'contacto_emergencia' => '0985562587',
+            'contacto_emergencia' => '0981044784',
             'clave' => password_hash('12345', PASSWORD_DEFAULT),
             'permisos' => 16,
             'rol' => 'Administrador'
